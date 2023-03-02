a5d7f6c5254e8d2d43eadeb86f118d5a0ff754ac
Talita Santos <talita.santos@ipdec.org>
2023-03-02 15:19:45 -0400

    feat: adicionado tema claro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>teste</Button>}></Route>
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            teste
+          </Button>
+        }
+      ></Route>
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
